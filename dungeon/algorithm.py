03575f54dcd0acd6b72602cebf3afb9c38beb7c8
blueloveTH <blueloveTH@foxmail.com>
2023-08-29 23:13:09 +0800

    ...

--- dungeon/algorithm.py
+++ dungeon/algorithm.py
@@ -1,5 +1,7 @@
 """Common algorithms for dungeon generation."""
 
+import heapq
+
 class Rect:
     def __init__(self, left: int, top: int, right: int, bottom: int):
         self.left = left
@@ -23,3 +25,23 @@ class Rect:
             min(self.right, other.right),
             min(self.bottom, other.bottom)
         )
+    
+
+# duck-type protocol
+class _NodeLike:
+    def get_neighbours(self) -> list[tuple('_NodeLike', int)]:
+        raise NotImplementedError
+
+def dijkstra(G: list[_NodeLike], node: _NodeLike) -> dict[_NodeLike, int]:
+    """Dijkstra's algorithm for finding the shortest path from a node to all other nodes in a graph."""
+    dist = {node: 0}
+    queue = [(0, node)]
+    while queue:
+        d, u = heapq.heappop(queue)
+        if d > dist[u]:
+            continue
+        for v, w in u.get_neighbours():
+            if v not in dist or dist[v] > dist[u] + w:
+                dist[v] = dist[u] + w
+                heapq.heappush(queue, (dist[v], v))
+    return dist
--- dungeon/pixeldungeon/__init__.py
+++ dungeon/pixeldungeon/__init__.py
@@ -1,6 +1,6 @@
 import random, math
 from dungeon.level import Level
-from dungeon.algorithm import Rect
+from dungeon.algorithm import Rect, dijkstra
 
 class _RectSplitter:
     def __init__(self, min_size: int, max_size: int):
@@ -35,6 +35,18 @@ class Room:
     def __init__(self, rect: Rect):
         self.rect = rect
         self.neighbours = []
+        self.connected = {}
+        self.price = 1
+        self.type = None
+
+    def get_neighbours(self) -> list[tuple('_NodeLike', int)]:
+        return [(nb, nb.price) for nb in self.neighbours]
+    
+    def connect(self, other: 'Room') -> None:
+        if other in self.connected:
+            return
+        self.connected[other] = None
+        other.connected[self] = None
 
 class PixelDungeonLevel(Level):
     """生成 Pixel Dungeon 地图的算法"""
@@ -42,6 +54,13 @@ class PixelDungeonLevel(Level):
     def __init__(self, width: int, height: int):
         super(PixelDungeonLevel, self).__init__(width, height)
         self.rooms = None
+
+    def get_greedy_path(self, dist: dict[Room, int], start: Room, end: Room) -> list[Room]:
+        """从 `start` 到 `end` 的贪心路径"""
+        path = [start]
+        while path[-1] is not end:
+            path.append(min(path[-1].neighbours, key=lambda nb: dist[nb]))
+        return path
 
     def build(self) -> bool:
         # split the level into rooms
@@ -64,6 +83,7 @@ class PixelDungeonLevel(Level):
                     room_j.neighbours.append(room_i)
 
         # determine the entrance and exit rooms
+        dist = None
         min_distance = int(math.sqrt(len(self.rooms)))
         for _ in range(10):
             while True:
@@ -77,10 +97,42 @@ class PixelDungeonLevel(Level):
                 if room_exit is not room_entrance and room_exit.width() >= 4 and room_exit.height() >= 4:
                     break
             # make sure the distance between `room_entrance` and `room_exit` is big enough
-            Graph.build_distance_map(self.rooms, room_exit)
-            if room_entrance.distance() >= min_distance:
+            dist = dijkstra(self.rooms, room_exit)
+            if dist[room_entrance] >= min_distance:
                 break
         else:
             # failed to find a suitable start and end room after 10 tries
             return False
-            
+        
+        room_entrance.type = 'entrance'
+        room_exit.type = 'exit'
+
+        connected = set()
+        connected.add(room_entrance)
+        
+        path = self.get_greedy_path(dist, room_entrance, room_exit)
+        for i in range(len(path) - 1):
+            path[i].connect(path[i + 1])
+            connected.add(path[i + 1])
+
+        # reset the price of each room
+        for room in path:
+            room.price = dist[room]
+
+        dist = dijkstra(self.rooms, room_exit)
+        path = self.get_greedy_path(dist, room_exit, room_entrance)
+
+        for i in range(len(path) - 1):
+            path[i].connect(path[i + 1])
+            connected.add(path[i + 1])
+
+        n_connected = int(len(self.rooms) * random.uniform(0.5, 0.7))
+        connected = list(connected)
+        while len(connected) < n_connected:
+            cr: Room = random.choice(connected)
+            or_: Room = random.choice(cr.neighbours)
+            if or_ not in connected:
+                cr.connect(or_)
+                connected.add(or_)
+
+        return True
